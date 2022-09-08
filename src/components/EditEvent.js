5d9b018b8788a58758c4fb1acccf90d6a25fea65
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-08 12:50:49 +0300

    Added input fields for the 4 classNames

--- src/components/EditEvent.js
+++ src/components/EditEvent.js
@@ -1,6 +1,65 @@
 import React from 'react'
+
 export const EditEvent = () => {
   return (
-    <div>EditEvent</div>
+      //attendees input 
+    <form className='edit-event'>
+        <label className='form-label' htmlFor='attendees'>Attendees:</label>
+        <input
+        name="attendees"
+            type="number"
+            placeholder="Amount of people attending..."
+            //value={updatedAttendees}
+            //onChange ={handleAttendeesChange}
+            className="edit-event-input">
+        </input>
+
+        <br/>
+            <label className="form-label" htmlFor="event-type">Type of Event: </label>
+            <input 
+            name="event-type"
+            type="string"
+            placeholder="Type of event..."
+            //value={updatedEventType}
+            //onChange={handleEventTypeChange}
+            className="edit-event-input"
+            ></input>
+        <br/>
+        
+        <label className="form-label" htmlFor="event-type">Type of Event: </label>
+            <input 
+            name="event-type"
+            type="string"
+            placeholder="Type of event..."
+            //value={updatedEventType}
+            //onChange={handleEventTypeChange}
+            className="edit-event-input"
+            ></input>
+            <br/>
+        
+            <label className="form-label" htmlFor="price">Ticket Price: </label>
+            <input 
+            name="price"
+            type="number"
+            placeholder="Ticket price..."
+            //value={updatedPrice}
+            //onChange={handlePriceChange}
+            className="edit-event-input"
+            ></input>
+            <br/>
+
+            <label className="form-label" htmlFor="date">Date: </label>
+            <input 
+            name="date"
+            type="date"
+            placeholder="Event date..."
+            //value={updatedDate}
+            //onChange={handleDateChange}
+            className="edit-event-input"
+            ></input>
+            <br/>
+        <input className="save-button" type="submit" value="Save" />
+
+</form>
   )
 }
