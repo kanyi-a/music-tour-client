51908654663951ed369fa8804ddcbc733386fecd
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-08 14:27:40 +0300

    Fetching most-popular and busybee data

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,40 @@
 import React, {useState, useEffect}from 'react'
 
 export const Stats = () => {
+    const [popular, setPopular] = useState("")
+    const [busy, setBusy] = useState("")
+
+
+    useEffect(() => {
+        fetch('http://localhost:9292/most-popular')
+        .then(r => r.json())
+        .then(data => setPopular(data))
+      }, [])
+    const name = popular.split('-')[0]
+    const fans = popular.split('-')[1]
+
+    useEffect(() => {
+        fetch('http://localhost:9292/busy-bee')
+        .then(r => r.json())
+        .then(data => setBusy(data))
+      }, [])
+    
+
+
   return (
-    <div>Stats</div>
+    <> 
+    <h2> Artist Stats </h2>
+    <div className="stats">
+        <h3>Most Popular:</h3>
+        <h5>{name}</h5>
+        <h5>{fans}</h5>
+    </div>
+    <br/>
+    <br/>
+    <div className="stats">
+        <h3>busy bee:</h3>
+        <h5>{busy}</h5>
+    </div>
+    </>
   )
 }
