8102fafc5b06d99c11b66f8714d877ebdbdf983e
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-08 14:12:33 +0300

    updated the return of the Eventlist function

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,11 +1,23 @@
-import React{useState} from 'react'
+import React, {useState} from 'react'
 import EventCard from './EventCard';
 import Search from './Search';
 
 function EventList() {
   return (
-    <div>EventList</div>
+    <>
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    <div className="event-list">
+    {eventList}
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <div className="next-container">
+        <button className="next-button" onClick={handleClickMore}>Next</button>
+    </div>
+    </>
   )
 }
 
-export default EventList
+export default EventList;
