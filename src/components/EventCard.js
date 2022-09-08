4513c2f160c8b0a2c00e6ae62d0203b82fd9260c
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-08 14:08:53 +0300

    Added functions and props

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,12 +1,69 @@
 import React, {useState} from 'react'
 import EditEvent from './EditEvent';
 
-function EventCard() {
+function EventCard({data, handleDeleteEvent, handleUpdateEvent}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [visible, setVisible] = useState(true)
+    const {id, attendees, date, event_type, price, venue, user} = data
+    const {venue_name, address, capacity, phone } = venue
+    const {username} = user
+
+//HANDLE DELETE FUNCTION
+    function handleDelete(){
+        fetch(`http://localhost:9292/events/${id}`, { 
+          method: 'DELETE',
+        })
+        .then((r) => r.json())
+        .then((deletedEvent) => handleDeleteEvent(deletedEvent))
+      }
+  
+      const dateSplit = date.split('-')
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      let dateItem = new Date(dateSplit).toLocaleDateString('en-US', options);
+
+
   return (
   <div className='card'>
+        {visible ? 
+            (<div> {isEditing ? (
+        <EditEvent
+          id={id}
+          data={data}
+          setIsEditing={setIsEditing}
+          handleUpdateEvent={handleUpdateEvent}
+        />
+      ) : (
+        <>
+        <div className="delete-edit-buttons">
+        <button className="edit-btn" onClick={() => setIsEditing((isEditing) => !isEditing)}>
+              <span role="img" aria-label="edit">
+                ✏️
+              </span>
+        </button>
+        <button onClick={handleDelete} className="delete-event-btn">X</button>
+        <br/>
+        <br/>
+        </div>        
+        <h3>{username} {event_type}</h3>
+          <h5>{dateItem}</h5>
+          <div className="price">🎟️ ${price}</div>
+          <div className="attendees">Attendees: {attendees}</div>
+          <br/>
+          <h5 className="venue-info" onClick={() => setVisible(!visible)}>Venue Info</h5>
+      </>
+      )} </div>) : (
+        <div className="venue-card"> 
+          <h5 className="venue-info" onClick={() => setVisible(!visible)}>Concert Info</h5>
+          <h5>{venue_name}</h5>
+          <p className="address">Address: {address}</p>
+          <p className="capacity">Capacity: {capacity}</p>
+          <p className='phone'>{phone}</p>
+        </div>
 
+
+      )}
   </div>
   )
 }
 
-export default EventCard
+export default EventCard;
