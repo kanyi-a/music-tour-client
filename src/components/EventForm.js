e177948c725425c3ceb59fe928763ec6d5f86af3
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-08 14:33:05 +0300

    Imported useNavigate

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,16 @@
 import React, {useState, useEffect} from 'react'
 
 function EventForm() {
+
+    const [attendees, setAttendees] = useState("");
+    const [eventType, setEventType] = useState("");
+    const [price, setPrice] = useState("");
+    const [date, setDate] = useState("");
+    const [venueData, setVenueData] = useState([]);
+    const [venue, setVenue] = useState('')
+    let navigate = useNavigate(); 
+
+
   return (
     <div>EventForm</div>
   )
